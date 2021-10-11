ba676c244ca80ac43147d5c508902e8c49d4df40
Dave Doro <sirdoro1@gmail.com>
2021-10-11 16:54:59 +0100

    update on modifying registerController and view for tenants

--- app/Http/Controllers/Tenants/Auth/RegisterController.php
+++ app/Http/Controllers/Tenants/Auth/RegisterController.php
@@ -8,6 +8,9 @@ use App\Models\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Auth\Events\Registered;
 
 class RegisterController extends Controller
 {
@@ -44,6 +47,29 @@ class RegisterController extends Controller
     public function showRegistrationForm()
     {
         return view('tenants.auth.register');
+    }
+
+    /**
+     * Handle a registration request for the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\JsonResponse
+     */
+    public function register(Request $request)
+    {
+        $this->validator($request->all())->validate();
+
+        event(new Registered($user = $this->create($request->all())));
+
+        $this->guard()->login($user);
+
+        if ($response = $this->registered($request, $user)) {
+            return $response;
+        }
+
+        return $request->wantsJson()
+        ? new JsonResponse([], 201)
+        : redirect($this->redirectPath());
     }
 
     /**
